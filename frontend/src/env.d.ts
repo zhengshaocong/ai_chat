9780c20ffc436a9784ac4e0ea968083a10eb4bda
zhengshaocong <648303252@QQ.com>
2025-06-17 11:58:33 +0800

    调整配置

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,4 +1,10 @@
- /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
 
 interface ImportMetaEnv {
     readonly VITE_API_BASE_URL: string
